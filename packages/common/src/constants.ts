a82e60465263f863d669d8d5a79c170b40c9dc9c
Kaique Lisboa <kaique.lisboa4@gmail.com>
2026-01-05 19:12:06 -0300

    v0.19.0-alpha-3

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -290,7 +290,7 @@ export const VERSION_TIMEOUT = 30000;
 export const SCROLL_TIMEOUT = 100;
 export const ZOOM_STEP = 0.1;
 export const MIN_ZOOM = 0.5;
-export const MAX_ZOOM = 5;
+export const MAX_ZOOM = 4;
 export const HYPERLINK_TOOLTIP_DELAY = 300;
 
 // Report a user inactive after IDLE_THRESHOLD milliseconds
